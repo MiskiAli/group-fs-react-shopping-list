0bc29b85cca8e57c716ccf42dcbace499bc40fa5
Tim Miner <poeminminer1@gmail.com>
2022-07-07 14:51:44 -0500

    Created GET reqest

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.get('/', (req, res) => {
+    // !!!!!!!Will have to change later to combine Quantity and UNIT !!!!!!!!!
+    const queryText = `SELECT * FROM "items" ORDER BY "name";`;
+    pool.query(queryText)
+        .then((result) => {
+            console.log('Heres what your getting from the db:', result);
+            res.send(result.rows);    
+        })
+        .catch((error) => {
+            console.log(`ERROR IN ROUTER GET REQUEST QUERY: ${queryText}`, error);
+            res.sendStatus(500);
+    })
+})
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,12 +5,24 @@ import ShoppingList from '../ShoppingList/ShoppingList.jsx';
 import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
+import { response } from 'express';
 
 // see if they created a state array container?
 const [shoppingList, setShoppingList] = useState([]);
 
 const getShoppingList = () => {
-
+    axios({
+        method: 'GET',
+        url: '/list'
+    })
+        .then((response) => {
+            console.log('the client response is:', response);
+            console.log('here is the data from response:', response.data);
+            setShoppingList(response.data);
+        })
+        .catch(function (error) {
+            console.log('ERROR ON GET', error);
+        });
 }
 
 useEffect(() => {
