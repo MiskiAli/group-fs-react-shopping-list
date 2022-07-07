2da0bfa77e6b7fe77d4190d8547d7b85ef689c69
Tim Miner <poeminminer1@gmail.com>
2022-07-07 14:22:29 -0500

    more boilerplate

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import ShoppingForm from '../ShoppingForm/ShoppingForm.jsx';
 import ShoppingList from '../ShoppingList/ShoppingList.jsx';
-
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
+
+// see if they created a state array container?
+const [shoppingList, setShoppingList] = useState([]);
+
+const getShoppingList = () => {
+
+}
 
 useEffect(() => {
     console.log('get here');
